9d06929b7b3f01834c11fbe9df9b3ccca0080117
Maciej Ostrowski <maciej.ostrowski@fremtind.no>
2024-02-08 08:58:56 +0100

    init

---
+++ src/data.ts
@@ -0,0 +1,1008 @@
+type Person = {
+	firstName: string
+	lastName: string
+	ssn: string
+}
+
+export const people: Person[] = [
+	{
+		firstName: 'OVERNATURLIG',
+		lastName: 'OPPLAG',
+		ssn: '13929797358',
+	},
+	{
+		firstName: 'KJÆR',
+		lastName: 'BYSSE',
+		ssn: '25877798731',
+	},
+	{
+		firstName: 'LAV',
+		lastName: 'HUMOR',
+		ssn: '26836499785',
+	},
+	{
+		firstName: 'OPPKLARENDE',
+		lastName: 'MALERBUKSE',
+		ssn: '03837697866',
+	},
+	{
+		firstName: 'USJENERT',
+		lastName: 'SKY',
+		ssn: '27856798106',
+	},
+	{
+		firstName: 'UTÅLMODIG',
+		lastName: 'EPLEKAKE',
+		ssn: '12867698823',
+	},
+	{
+		firstName: 'REFLEKSIV',
+		lastName: 'ERIDOFOBI',
+		ssn: '27849198861',
+	},
+	{
+		firstName: 'ALVORLIG',
+		lastName: 'OSTEHØVEL',
+		ssn: '02846197551',
+	},
+	{
+		firstName: 'USENSUELL',
+		lastName: 'KJØTTDEIG',
+		ssn: '11916998726',
+	},
+	{
+		firstName: 'OPPLYST',
+		lastName: 'KARRIERE',
+		ssn: '18888499527',
+	},
+	{
+		firstName: 'UNDERDANIG',
+		lastName: 'TROMPET',
+		ssn: '13877699565',
+	},
+	{
+		firstName: 'GØYAL',
+		lastName: 'SÅPE',
+		ssn: '04887596889',
+	},
+	{
+		firstName: 'MUSIKALSK',
+		lastName: 'AVDELING',
+		ssn: '11889498899',
+	},
+	{
+		firstName: 'UMUSIKALSK',
+		lastName: 'TANNBØRSTE',
+		ssn: '01896697567',
+	},
+	{
+		firstName: 'ALLSLAGS',
+		lastName: 'BAR',
+		ssn: '10867295300',
+	},
+	{
+		firstName: 'STOLT',
+		lastName: 'ALV',
+		ssn: '19858298256',
+	},
+	{
+		firstName: 'FORNUFTIG',
+		lastName: 'KOKEPLATE',
+		ssn: '19856197657',
+	},
+	{
+		firstName: 'TØRR',
+		lastName: 'JEANS',
+		ssn: '08827099857',
+	},
+	{
+		firstName: 'LEDENDE',
+		lastName: 'FILMOMTALE',
+		ssn: '11826299535',
+	},
+	{
+		firstName: 'HYPPIG',
+		lastName: 'HENSIKT',
+		ssn: '07880098300',
+	},
+	{
+		firstName: 'LEKKER',
+		lastName: 'JUNI',
+		ssn: '06819799871',
+	},
+	{
+		firstName: 'AKUSTISK',
+		lastName: 'ALDERDOM',
+		ssn: '13816198466',
+	},
+	{
+		firstName: 'PRIKKETE',
+		lastName: 'STRUTS',
+		ssn: '22848497774',
+	},
+	{
+		firstName: 'SALT',
+		lastName: 'HETE',
+		ssn: '29926795943',
+	},
+	{
+		firstName: 'MOMENTAN',
+		lastName: 'VALMUE',
+		ssn: '02868397953',
+	},
+	{
+		firstName: 'KLARTENKT',
+		lastName: 'TUNNEL',
+		ssn: '15908197655',
+	},
+	{
+		firstName: 'KVART',
+		lastName: 'FEBER',
+		ssn: '14926897595',
+	},
+	{
+		firstName: 'AKSEPTABEL',
+		lastName: 'KAMERA',
+		ssn: '11877296204',
+	},
+	{
+		firstName: 'PLUTSELIG',
+		lastName: 'JUBEL',
+		ssn: '28928896422',
+	},
+	{
+		firstName: 'DEILIG',
+		lastName: 'OPPHOPING',
+		ssn: '02816298043',
+	},
+	{
+		firstName: 'PESSIMISTISK',
+		lastName: 'KUTTE',
+		ssn: '31888199139',
+	},
+	{
+		firstName: 'KORT',
+		lastName: 'DUK',
+		ssn: '27927194932',
+	},
+	{
+		firstName: 'MANGE',
+		lastName: 'ALDERSFORSKJELL',
+		ssn: '20917297704',
+	},
+	{
+		firstName: 'RETTFERDIG',
+		lastName: 'SATIRE',
+		ssn: '26887897726',
+	},
+	{
+		firstName: 'ELASTISK',
+		lastName: 'AVSLUTNINGSFEST',
+		ssn: '21819898046',
+	},
+	{
+		firstName: 'UROKKELIG',
+		lastName: 'TIRSDAG',
+		ssn: '20916398590',
+	},
+	{
+		firstName: 'TALEFØR',
+		lastName: 'PIANIST',
+		ssn: '01916399614',
+	},
+	{
+		firstName: 'TRU',
+		lastName: 'BEVEGELSE',
+		ssn: '23846996958',
+	},
+	{
+		firstName: 'HEL',
+		lastName: 'AKTUALITET',
+		ssn: '14867097911',
+	},
+	{
+		firstName: 'STOR',
+		lastName: 'HEIS',
+		ssn: '19847997713',
+	},
+	{
+		firstName: 'SMISKENDE',
+		lastName: 'HUMOR',
+		ssn: '15907595185',
+	},
+	{
+		firstName: 'ALLSLAGS',
+		lastName: 'BYREGION',
+		ssn: '09900199020',
+	},
+	{
+		firstName: 'UNDERDANIG',
+		lastName: 'SUM',
+		ssn: '30857096629',
+	},
+	{
+		firstName: 'MOMENTAN',
+		lastName: 'TOWNSHIP',
+		ssn: '20916997702',
+	},
+	{
+		firstName: 'RETT',
+		lastName: 'FLASKE',
+		ssn: '14849997956',
+	},
+	{
+		firstName: 'KOMPLETT',
+		lastName: 'SLEKTNING',
+		ssn: '22858298192',
+	},
+	{
+		firstName: 'MYE',
+		lastName: 'MAST',
+		ssn: '10838699001',
+	},
+	{
+		firstName: 'PLEIENDE',
+		lastName: 'AMBASSADEFUNKSJONÆR',
+		ssn: '22826694703',
+	},
+	{
+		firstName: 'USEDVANLIG',
+		lastName: 'BØK',
+		ssn: '21829898621',
+	},
+	{
+		firstName: 'POPULÆR',
+		lastName: 'FORMIDDAG',
+		ssn: '04928397829',
+	},
+	{
+		firstName: 'FLAT',
+		lastName: 'BANK',
+		ssn: '30916098509',
+	},
+	{
+		firstName: 'GUL',
+		lastName: 'PARYKK',
+		ssn: '16886597189',
+	},
+	{
+		firstName: 'MUSKULØS',
+		lastName: 'BÅNDSALAT',
+		ssn: '15928599105',
+	},
+	{
+		firstName: 'OPPFYLLENDE',
+		lastName: 'AVTALE',
+		ssn: '03817298191',
+	},
+	{
+		firstName: 'ELEGANT',
+		lastName: 'FASTLANDSFORBINDELSE',
+		ssn: '28888997077',
+	},
+	{
+		firstName: 'VAKLENDE',
+		lastName: 'LAMPETT',
+		ssn: '13910198607',
+	},
+	{
+		firstName: 'RASTLØS',
+		lastName: 'BAKGRUNNSLITTERATUR',
+		ssn: '18837399528',
+	},
+	{
+		firstName: 'KORREKT',
+		lastName: 'MAST',
+		ssn: '01856697092',
+	},
+	{
+		firstName: 'UMAKE',
+		lastName: 'HALVSØSKEN',
+		ssn: '24896299200',
+	},
+	{
+		firstName: 'KONGE',
+		lastName: 'NETTHINNE',
+		ssn: '10856998709',
+	},
+	{
+		firstName: 'MEDGJØRLIG',
+		lastName: 'ETTERMIDDAG',
+		ssn: '23876497999',
+	},
+	{
+		firstName: 'BLID',
+		lastName: 'TREKKFUGL',
+		ssn: '06849099068',
+	},
+	{
+		firstName: 'SORGLØS',
+		lastName: 'PRODUKSJON',
+		ssn: '11836595566',
+	},
+	{
+		firstName: 'TILLITSFULL',
+		lastName: 'KJEKS',
+		ssn: '23827998907',
+	},
+	{
+		firstName: 'FIKTIV',
+		lastName: 'LEFSE',
+		ssn: '06816799110',
+	},
+	{
+		firstName: 'OVERBEVISENDE',
+		lastName: 'HALVØY',
+		ssn: '01867296405',
+	},
+	{
+		firstName: 'RETT',
+		lastName: 'LEVEREGEL',
+		ssn: '06858197706',
+	},
+	{
+		firstName: 'TØFF',
+		lastName: 'SYND',
+		ssn: '12920197874',
+	},
+	{
+		firstName: 'AKUSTISK',
+		lastName: 'SKO',
+		ssn: '17847898275',
+	},
+	{
+		firstName: 'OPPBLÅST',
+		lastName: 'DIESEL',
+		ssn: '15878999271',
+	},
+	{
+		firstName: 'MORALSK',
+		lastName: 'DAGKREM',
+		ssn: '03916896459',
+	},
+	{
+		firstName: 'URETTFERDIG',
+		lastName: 'PÅKJENNING',
+		ssn: '03848497761',
+	},
+	{
+		firstName: 'VARSOM',
+		lastName: 'ALV',
+		ssn: '25908598302',
+	},
+	{
+		firstName: 'INTELLIGENT',
+		lastName: 'PYJAMASBUKSE',
+		ssn: '24847097155',
+	},
+	{
+		firstName: 'SPESIFIKK',
+		lastName: 'AVDELING',
+		ssn: '17876695561',
+	},
+	{
+		firstName: 'KUNNSKAPSLØS',
+		lastName: 'ALTMULIGMANN',
+		ssn: '13887297228',
+	},
+	{
+		firstName: 'FORSTANDIG',
+		lastName: 'PERMANENT',
+		ssn: '14906299349',
+	},
+	{
+		firstName: 'TØRST',
+		lastName: 'RESSURS',
+		ssn: '08830097334',
+	},
+	{
+		firstName: 'IVRIG',
+		lastName: 'SYND',
+		ssn: '20879397198',
+	},
+	{
+		firstName: 'AKROBATISK',
+		lastName: 'RETORIKK',
+		ssn: '07916098892',
+	},
+	{
+		firstName: 'EVENTYRLIG',
+		lastName: 'FASE',
+		ssn: '05889999138',
+	},
+	{
+		firstName: 'SLEM',
+		lastName: 'ENTREPRENØR',
+		ssn: '29858099917',
+	},
+	{
+		firstName: 'NERVØS',
+		lastName: 'AVSTAND',
+		ssn: '02898499289',
+	},
+	{
+		firstName: 'HORISONTAL',
+		lastName: 'ENERGI',
+		ssn: '23848398152',
+	},
+	{
+		firstName: 'SKÅNSOM',
+		lastName: 'AMBOLT',
+		ssn: '25858696405',
+	},
+	{
+		firstName: 'KORREKT',
+		lastName: 'KONSONANT',
+		ssn: '20907699618',
+	},
+	{
+		firstName: 'AUTORISERT',
+		lastName: 'KLUT',
+		ssn: '27836697664',
+	},
+	{
+		firstName: 'EKSAKT',
+		lastName: 'SERVITRISE',
+		ssn: '16926199114',
+	},
+	{
+		firstName: 'SPESIFIKK',
+		lastName: 'TAKKE',
+		ssn: '30816296400',
+	},
+	{
+		firstName: 'SVÆR',
+		lastName: 'RAMME',
+		ssn: '17906295334',
+	},
+	{
+		firstName: 'INKLUDERENDE',
+		lastName: 'DUELL',
+		ssn: '14867398415',
+	},
+	{
+		firstName: 'LIDENSKAPELIG',
+		lastName: 'GRAVIMETRI',
+		ssn: '01880098124',
+	},
+	{
+		firstName: 'AKROBATISK',
+		lastName: 'LUNSJ',
+		ssn: '14837996792',
+	},
+	{
+		firstName: 'INTERESSANT',
+		lastName: 'GAUPE',
+		ssn: '04897699694',
+	},
+	{
+		firstName: 'UBESTIKKELIG',
+		lastName: 'MATVARE',
+		ssn: '11879299731',
+	},
+	{
+		firstName: 'FREIDIG',
+		lastName: 'BØTTE',
+		ssn: '24858299469',
+	},
+	{
+		firstName: 'SKRIVEFØR',
+		lastName: 'PLATE',
+		ssn: '28897698384',
+	},
+	{
+		firstName: 'OPPSTEMT',
+		lastName: 'NETTVERKSKABEL',
+		ssn: '07898599951',
+	},
+	{
+		firstName: 'PROAKTIV',
+		lastName: 'BAKTANKE',
+		ssn: '07847898628',
+	},
+	{
+		firstName: 'UKONTROVERSIELL',
+		lastName: 'STØVEL',
+		ssn: '07926895782',
+	},
+	{
+		firstName: 'UINTERESSERT',
+		lastName: 'SEKRETÆR',
+		ssn: '28898298843',
+	},
+	{
+		firstName: 'ULIK',
+		lastName: 'INTRIKAT',
+		ssn: '23838197256',
+	},
+	{
+		firstName: 'FIOLETT',
+		lastName: 'HINNE',
+		ssn: '24889297960',
+	},
+	{
+		firstName: 'RØD',
+		lastName: 'PIZZA',
+		ssn: '09887598875',
+	},
+	{
+		firstName: 'REDD',
+		lastName: 'AVANSE',
+		ssn: '01887195257',
+	},
+	{
+		firstName: 'LILLA',
+		lastName: 'AVENY',
+		ssn: '02870098173',
+	},
+	{
+		firstName: 'TRÅDLØS',
+		lastName: 'BØR',
+		ssn: '16858997139',
+	},
+	{
+		firstName: 'DEILIG',
+		lastName: 'KILE',
+		ssn: '20897599164',
+	},
+	{
+		firstName: 'OVERMODIG',
+		lastName: 'DIALEKT',
+		ssn: '23837596831',
+	},
+	{
+		firstName: 'SLEM',
+		lastName: 'PUTE',
+		ssn: '09836595154',
+	},
+	{
+		firstName: 'ALMINNELIG',
+		lastName: 'DUNKJEVLE',
+		ssn: '18859198062',
+	},
+	{
+		firstName: 'OVERFLØDIG',
+		lastName: 'GLOSE',
+		ssn: '22817199481',
+	},
+	{
+		firstName: 'LETT',
+		lastName: 'PAPPA',
+		ssn: '26927699965',
+	},
+	{
+		firstName: 'TROSSIG',
+		lastName: 'SOKK',
+		ssn: '14906498759',
+	},
+	{
+		firstName: 'KRANGLETE',
+		lastName: 'KJÆRESTE',
+		ssn: '29916396501',
+	},
+	{
+		firstName: 'VELKOMMEN',
+		lastName: 'GLUTEN',
+		ssn: '19840098470',
+	},
+	{
+		firstName: 'RØD',
+		lastName: 'KAMEL',
+		ssn: '27857496847',
+	},
+	{
+		firstName: 'DOBBEL',
+		lastName: 'NATUR',
+		ssn: '13827197933',
+	},
+	{
+		firstName: 'INTUITIV',
+		lastName: 'VARE',
+		ssn: '25888498101',
+	},
+	{
+		firstName: 'LOJAL',
+		lastName: 'FASTE',
+		ssn: '20879196443',
+	},
+	{
+		firstName: 'MISFORNØYD',
+		lastName: 'FLYVERTINNE',
+		ssn: '11869399258',
+	},
+	{
+		firstName: 'FIKTIV',
+		lastName: 'GARASJE',
+		ssn: '02828497742',
+	},
+	{
+		firstName: 'TØRR',
+		lastName: 'KONTINGENT',
+		ssn: '31900199646',
+	},
+	{
+		firstName: 'OPTIMISTISK',
+		lastName: 'OTER',
+		ssn: '28837398039',
+	},
+	{
+		firstName: 'RAKRYGGET',
+		lastName: 'TALLTEGN',
+		ssn: '18889898492',
+	},
+	{
+		firstName: 'FANTASILØS',
+		lastName: 'MOR',
+		ssn: '29897399235',
+	},
+	{
+		firstName: 'KONSEKVENT',
+		lastName: 'KRYPDYR',
+		ssn: '11816396419',
+	},
+	{
+		firstName: 'PRAGMATISK',
+		lastName: 'BRETT',
+		ssn: '10909797956',
+	},
+	{
+		firstName: 'LANGFINGRET',
+		lastName: 'PAUSE',
+		ssn: '22866698604',
+	},
+	{
+		firstName: 'HÅRSÅR',
+		lastName: 'INFORMASJON',
+		ssn: '24907799170',
+	},
+	{
+		firstName: 'STORARTET',
+		lastName: 'DEL',
+		ssn: '19928099671',
+	},
+	{
+		firstName: 'FRISK',
+		lastName: 'FIOLIN',
+		ssn: '24817499836',
+	},
+	{
+		firstName: 'PERFEKT',
+		lastName: 'VAFFEL',
+		ssn: '01878099377',
+	},
+	{
+		firstName: 'OPERATIV',
+		lastName: 'KRABBE',
+		ssn: '25828995785',
+	},
+	{
+		firstName: 'RIKTIG',
+		lastName: 'FOXTROT',
+		ssn: '14828999525',
+	},
+	{
+		firstName: 'UGLESETT',
+		lastName: 'MEDISTERDEIG',
+		ssn: '21928099498',
+	},
+	{
+		firstName: 'HENSYNSFULL',
+		lastName: 'LIKNING',
+		ssn: '17858299667',
+	},
+	{
+		firstName: 'TROFAST',
+		lastName: 'KAKAOBØNNE',
+		ssn: '30888799105',
+	},
+	{
+		firstName: 'NYSGJERRIG',
+		lastName: 'ONKEL',
+		ssn: '07907696582',
+	},
+	{
+		firstName: 'ALTERNATIV',
+		lastName: 'BÆRTUR',
+		ssn: '26888197647',
+	},
+	{
+		firstName: 'LIVSGLAD',
+		lastName: 'JORD',
+		ssn: '30918499322',
+	},
+	{
+		firstName: 'MATEMATISK',
+		lastName: 'BAKGRUNNSLITTERATUR',
+		ssn: '15906298241',
+	},
+	{
+		firstName: 'ORDENTLIG',
+		lastName: 'UNIVERSITET',
+		ssn: '29878197583',
+	},
+	{
+		firstName: 'GJENNOMSIKTIG',
+		lastName: 'VITS',
+		ssn: '04896499977',
+	},
+	{
+		firstName: 'DYP',
+		lastName: 'STUND',
+		ssn: '21896098044',
+	},
+	{
+		firstName: 'REDD',
+		lastName: 'FLOD',
+		ssn: '23907097013',
+	},
+	{
+		firstName: 'PÅTROPPENDE',
+		lastName: 'KOMFYR',
+		ssn: '06896798861',
+	},
+	{
+		firstName: 'KONSERVATIV',
+		lastName: 'LINJAL',
+		ssn: '19928399462',
+	},
+	{
+		firstName: 'SELVHJULPEN',
+		lastName: 'RESSURS',
+		ssn: '30918299714',
+	},
+	{
+		firstName: 'POPULÆR',
+		lastName: 'BUKK',
+		ssn: '04919498752',
+	},
+	{
+		firstName: 'MÅTEHOLDEN',
+		lastName: 'APPETITT',
+		ssn: '24877699818',
+	},
+	{
+		firstName: 'VIKTIG',
+		lastName: 'BAND',
+		ssn: '06887299356',
+	},
+	{
+		firstName: 'UNDERFUNDIG',
+		lastName: 'MOLDVARP',
+		ssn: '19918196636',
+	},
+	{
+		firstName: 'HORISONTAL',
+		lastName: 'GJERRIGKNARK',
+		ssn: '11847799880',
+	},
+	{
+		firstName: 'URIMELIG',
+		lastName: 'GALLUPMÅLING',
+		ssn: '05887399095',
+	},
+	{
+		firstName: 'KOMPLETT',
+		lastName: 'FLAMME',
+		ssn: '04826697968',
+	},
+	{
+		firstName: 'SJELDEN',
+		lastName: 'SYSTRENDING',
+		ssn: '06906898487',
+	},
+	{
+		firstName: 'GJENSIDIG',
+		lastName: 'KRÅKE',
+		ssn: '20888595938',
+	},
+	{
+		firstName: 'LEVENDE',
+		lastName: 'ABONNEMENT',
+		ssn: '09928596890',
+	},
+	{
+		firstName: 'USEDVANLIG',
+		lastName: 'LIVSSTIL',
+		ssn: '19878398338',
+	},
+	{
+		firstName: 'OVEREKSPONERT',
+		lastName: 'AKADEMISK',
+		ssn: '23929697650',
+	},
+	{
+		firstName: 'BLØT',
+		lastName: 'TALLERKEN',
+		ssn: '03886995910',
+	},
+	{
+		firstName: 'FLYKTIG',
+		lastName: 'UNGDOM',
+		ssn: '23879198895',
+	},
+	{
+		firstName: 'EMPIRISK',
+		lastName: 'PURRE',
+		ssn: '03917999111',
+	},
+	{
+		firstName: 'HEL',
+		lastName: 'LEVEREGEL',
+		ssn: '16886497087',
+	},
+	{
+		firstName: 'PRIKKETE',
+		lastName: 'TRENCHCOAT',
+		ssn: '19867398824',
+	},
+	{
+		firstName: 'SPONTAN',
+		lastName: 'FELTTOG',
+		ssn: '01856498944',
+	},
+	{
+		firstName: 'KVADRATISK',
+		lastName: 'LUPE',
+		ssn: '08898497535',
+	},
+	{
+		firstName: 'LIKEGYLDIG',
+		lastName: 'SAUS',
+		ssn: '26828099859',
+	},
+	{
+		firstName: 'FORSIKTIG',
+		lastName: 'OMELETT',
+		ssn: '28929998563',
+	},
+	{
+		firstName: 'VAKLENDE',
+		lastName: 'BARON',
+		ssn: '12867497836',
+	},
+	{
+		firstName: 'SPETTETE',
+		lastName: 'ENERGI',
+		ssn: '18906297627',
+	},
+	{
+		firstName: 'LAV',
+		lastName: 'GLADIATOR',
+		ssn: '30878297381',
+	},
+	{
+		firstName: 'KONGE',
+		lastName: 'OPPTAKSPRØVE',
+		ssn: '14899298854',
+	},
+	{
+		firstName: 'FILOSOFISK',
+		lastName: 'KRÅKESØLV',
+		ssn: '30908799476',
+	},
+	{
+		firstName: 'FORNØYD',
+		lastName: 'ANSIKTSKREM',
+		ssn: '14836694962',
+	},
+	{
+		firstName: 'OPPRETT',
+		lastName: 'REISE',
+		ssn: '23866299110',
+	},
+	{
+		firstName: 'MOMENTAN',
+		lastName: 'NØTT',
+		ssn: '06838399264',
+	},
+	{
+		firstName: 'SLAKK',
+		lastName: 'DOKTORGRAD',
+		ssn: '23866198810',
+	},
+	{
+		firstName: 'OPPRIKTIG',
+		lastName: 'FRUKT',
+		ssn: '06816196491',
+	},
+	{
+		firstName: 'ALLSIDIG',
+		lastName: 'POTET',
+		ssn: '05886797887',
+	},
+	{
+		firstName: 'OVERSIKTLIG',
+		lastName: 'DANS',
+		ssn: '30859596408',
+	},
+	{
+		firstName: 'UMUSIKALSK',
+		lastName: 'PÅSKEEGG',
+		ssn: '08890199659',
+	},
+	{
+		firstName: 'PRATSOM',
+		lastName: 'SYNSING',
+		ssn: '21857899277',
+	},
+	{
+		firstName: 'ALLSLAGS',
+		lastName: 'TELEFONNUMMER',
+		ssn: '30906399799',
+	},
+	{
+		firstName: 'AKUSTISK',
+		lastName: 'HANDELSFLÅTE',
+		ssn: '13888997959',
+	},
+	{
+		firstName: 'VENSTRE',
+		lastName: 'RYNKE',
+		ssn: '07919497895',
+	},
+	{
+		firstName: 'OPERATIV',
+		lastName: 'KAR',
+		ssn: '27827798911',
+	},
+	{
+		firstName: 'NYTTIG',
+		lastName: 'AMBULANSE',
+		ssn: '11877997962',
+	},
+	{
+		firstName: 'KONSERVATIV',
+		lastName: 'ORIDÉ',
+		ssn: '01837398467',
+	},
+	{
+		firstName: 'OPTIMISTISK',
+		lastName: 'HANKATT',
+		ssn: '24837797434',
+	},
+	{
+		firstName: 'VENNLIG',
+		lastName: 'SEIDEL',
+		ssn: '11869999182',
+	},
+	{
+		firstName: 'USEDVANLIG',
+		lastName: 'VARE',
+		ssn: '14869498654',
+	},
+	{
+		firstName: 'RESERVERT',
+		lastName: 'PAPEGØYE',
+		ssn: '30927599577',
+	},
+	{
+		firstName: 'NYTTIG',
+		lastName: 'PÅFUNN',
+		ssn: '26827096783',
+	},
+	{
+		firstName: 'AUTORISERT',
+		lastName: 'RIST',
+		ssn: '10898399341',
+	},
+	{
+		firstName: 'MINST',
+		lastName: 'HORISONT',
+		ssn: '12876599234',
+	},
+	{
+		firstName: 'KONSEKVENT',
+		lastName: 'LENSMANN',
+		ssn: '24896098809',
+	},
+	{
+		firstName: 'OPPFARENDE',
+		lastName: 'FAMILIE',
+		ssn: '07886999717',
+	},
+	{
+		firstName: 'SOLID',
+		lastName: 'VEVSTOL',
+		ssn: '23858698448',
+	},
+]
